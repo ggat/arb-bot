49db1bd2fd7f0fc4122b64e5aae84261adabf8a9
Giga Gatenashvili <gatenashvili@galaxystudio.ge>
2017-12-02 12:31:41 +0400

    - Fixes to pom files.

---
+++ Daemon/src/main/java/ge/shitbot/daemon/Launch.java
@@ -0,0 +1,17 @@
+package ge.shitbot.daemon;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by giga on 12/2/17.
+ */
+public class Launch {
+
+    private static Logger logger = LoggerFactory.getLogger(Launch.class);
+
+    public static void main(String[] args) {
+        //
+        logger.info("Daemon started.");
+    }
+}
