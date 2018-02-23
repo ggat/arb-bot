518a9755b00577f115b085d2434e72b0dd020e35
Giga <g.gatenashvili@gmail.com>
2018-02-23 15:15:25 +0400

    - New working scraping method for CrystalBet. May need completions.
    - Added posibility to load file directly as string in Resources utility class.

--- Core/src/main/java/ge/shitbot/core/datatypes/util/Resources.java
+++ Core/src/main/java/ge/shitbot/core/datatypes/util/Resources.java
@@ -1,5 +1,6 @@
 package ge.shitbot.core.datatypes.util;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -19,5 +20,9 @@ public class Resources {
 
     public static String toAbsName(URL url) throws URISyntaxException {
         return Paths.get(url.toURI()).toAbsolutePath().toString();
+    }
+
+    public static String fromRootAsString(Object referer, String resource) throws IOException {
+        return StringUtils.fromStream(loadFromRoot(referer, resource).openStream());
     }
 }
